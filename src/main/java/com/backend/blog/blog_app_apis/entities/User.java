d0f4be858e348c370ab5173880cb216c94339597
Tushar Porje <tusharporje13091999@gmail.com>
2024-05-28 23:56:02 +0530

    -

--- src/main/java/com/backend/blog/blog_app_apis/entities/User.java
+++ src/main/java/com/backend/blog/blog_app_apis/entities/User.java
@@ -47,34 +47,5 @@ public class User{
                                     inverseJoinColumns = @JoinColumn(name="role",referencedColumnName = "id"))
 	private Set<Role> roles = new HashSet<>();
 
-    // @Override
-    // public Collection<? extends GrantedAuthority> getAuthorities() {
-    //     List<SimpleGrantedAuthority> authority =roles.stream().map((role)->new SimpleGrantedAuthority(role.getName())).toList();
-    //     return authority;
-    // }
-
-    // @Override
-    // public String getUsername() {
-    //     return this.email;
-    // }
-
-    // @Override
-    // public boolean isAccountNonExpired() {
-    //     return true;
-    // }
-
-    // @Override
-    // public boolean isAccountNonLocked() {
-    //     return true;
-    // }
-
-    // @Override
-    // public boolean isCredentialsNonExpired() {
-    //     return true;
-    // }
-
-    // @Override
-    // public boolean isEnabled() {
-    //     return true;
-    // }
+    
 }
---
+++ src/main/java/com/backend/blog/blog_app_apis/payloads/RoleDto.java
@@ -0,0 +1,11 @@
+package com.backend.blog.blog_app_apis.payloads;
+
+
+import lombok.Data;
+
+@Data
+public class RoleDto {
+    
+    int id;
+    String name;
+}
