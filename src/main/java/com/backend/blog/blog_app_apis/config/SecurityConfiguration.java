fb0c7ea8683baa0add46aef8de9790131690dece
Tushar Porje <tusharporje13091999@gmail.com>
2024-05-30 23:05:34 +0530

    setup different environment profile

--- src/main/java/com/backend/blog/blog_app_apis/config/SecurityConfiguration.java
+++ src/main/java/com/backend/blog/blog_app_apis/config/SecurityConfiguration.java
@@ -7,31 +7,30 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.backend.blog.blog_app_apis.security.JwtAuthenticationEntryPoint;
 import com.backend.blog.blog_app_apis.security.JwtAuthenticationFilter;
-import com.backend.blog.blog_app_apis.security.MyUserDetailService;
 
 @Configuration
 // @EnableWebSecurity
-@EnableMethodSecurity//added bcz used @PreAuthorize("hasRole('ADMIN')") on getAllUser controller method
-public class SecurityConfiguration{
-    
-    
+@EnableWebMvc
+@EnableMethodSecurity // added bcz used @PreAuthorize("hasRole('ADMIN')") on getAllUser controller
+                      // method
+public class SecurityConfiguration {
+
     // @Bean
     // UserDetailsService userDetailsService(){
-    //     return new MyUserDetailService();
+    // return new MyUserDetailService();
     // }
 
     // @Bean
     // PasswordEncoder passwordEncoder(){
-    //     return new BCryptPasswordEncoder();
+    // return new BCryptPasswordEncoder();
     // }
 
     @Autowired
@@ -41,23 +40,27 @@ public class SecurityConfiguration{
     private JwtAuthenticationFilter filter;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        final String[] PUBLIC_URLS = {
+                "/api/auth/**", "/v3/api-docs","/v2/api-docs", "/swagger-resources/**", "/swagger-ui/**", "webjars/**"
+        };
+
         httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests()
                 // .requestMatchers("/api/posts/").hasRole("ADMIN")
                 // .requestMatchers("api/users/").hasRole("USER")
                 // .requestMatchers("api/categories/").hasRole("USER")
-                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers(PUBLIC_URLS).permitAll()
                 .anyRequest()
                 .authenticated()
                 .and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-                httpSecurity.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
-
 
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
